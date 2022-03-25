446dc0f9f69f9fbb9bcd521c8f1c4477398cd4b9
sonymegas <gnet1111@gmail.com>
2022-03-25 10:04:41 +0200

    Add files via upload

---
+++ stream1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+
+<title> Stream #1 </title>
+
+</head>
+
+<body>
+
+<iframe src="https://wigistream.to/embed/3sihbzngt39ny4" width="100%" height="100%"  scrolling="no" frameborder="0" allowfullscreen="true"></iframe>
+
+
+</body>
+
+
+</html>
